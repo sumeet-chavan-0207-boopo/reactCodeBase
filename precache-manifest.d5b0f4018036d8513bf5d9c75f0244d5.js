a485422e7a61916fba192fc5a623646eb2046e72
pushpa <pushpa.k@boppotechnologies.com>
2020-02-05 11:30:08 +0530

    Updates

---
+++ precache-manifest.d5b0f4018036d8513bf5d9c75f0244d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08a54c1d79804cc0cb4e5d0622e10385",
+    "url": "/reactCodeBase/index.html"
+  },
+  {
+    "revision": "b712b1e8670a1eb10826",
+    "url": "/reactCodeBase/static/css/2.8d392223.chunk.css"
+  },
+  {
+    "revision": "0f246dac9b17dc05786b",
+    "url": "/reactCodeBase/static/css/main.5958095c.chunk.css"
+  },
+  {
+    "revision": "b712b1e8670a1eb10826",
+    "url": "/reactCodeBase/static/js/2.fa6f1a25.chunk.js"
+  },
+  {
+    "revision": "8beea1ae56c20c39cf7a43c71f37c103",
+    "url": "/reactCodeBase/static/js/2.fa6f1a25.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0f246dac9b17dc05786b",
+    "url": "/reactCodeBase/static/js/main.509fc880.chunk.js"
+  },
+  {
+    "revision": "ae5e931fab7e4b9a550b",
+    "url": "/reactCodeBase/static/js/runtime-main.7b352cea.js"
+  }
+]);
